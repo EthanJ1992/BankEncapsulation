358263ea8dde6ecabf57930c5695082f65100bdd
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-14 17:33:55 -0500

    bank exercise

---
+++ BankEncapsulation/BankExercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankExercise
+    {
+        public class BankAccount
+        {
+            private double _balance = 0;
+            public void Deposite(double amount)
+            {
+               _balance += amount;
+                
+            } 
+            public double GetBalance()
+            {
+                return _balance;
+
+            }
+        }
+
+
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BankEncapsulation
+﻿using static BankEncapsulation.BankExercise;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myBank = new BankAccount();
+            Console.WriteLine("enter amount you wish to deposite");
+            var DepositeAmount = int.Parse(Console.ReadLine());
+            myBank.Deposite(DepositeAmount);
+            Console.WriteLine("Your balance is now:");
+            Console.WriteLine(myBank.GetBalance());
         }
     }
 }
